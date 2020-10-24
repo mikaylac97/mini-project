4d05cb8df2e4a9f01876a62a3f155e9f411ffe60
Mikayla Castro <mikaylacastro97@gmail.com>
2020-10-24 16:59:37 -0400

    changes

---
+++ build/precache-manifest.0c7598b0ed652b7b80883d30676f4912.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb3d6fafc399e2b10fa12bad5864688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1497682cfe46895d63a6",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "68492da95169b440391e",
+    "url": "/static/css/main.7503db28.chunk.css"
+  },
+  {
+    "revision": "1497682cfe46895d63a6",
+    "url": "/static/js/2.998cefcf.chunk.js"
+  },
+  {
+    "revision": "44b6ea107d5f6954b31204b3600b6c0e",
+    "url": "/static/js/2.998cefcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68492da95169b440391e",
+    "url": "/static/js/main.23cb9aab.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "819ff6381aef473b51076cf8336afb69",
+    "url": "/static/media/concert2.819ff638.jpg"
+  }
+]);
